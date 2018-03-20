8149e95f70c7b6dbfc98b1945397ed2fa8ef5151
Wes Ratcliff <wes.ratcliff@gmail.com>
2018-03-20 10:51:52 -0700

    feat: Ported to vue-cli

---
+++ src/components/Game.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+    <h1 v-show="hasWon === true">You won!</h1>
+    <h1 v-show="invalidMove === true">Pick another space!</h1>
+    <table>
+      <tr>
+        <td id="cell-1" @click="handleClick(0)">{{ moves[0] }}</td>
+        <td id="cell-2" @click="handleClick(1)">{{ moves[1] }}</td>
+        <td id="cell-3" @click="handleClick(2)">{{ moves[2] }}</td>
+      </tr>
+      <tr>
+        <td id="cell-4" @click="handleClick(3)">{{ moves[3] }}</td>
+        <td id="cell-5" @click="handleClick(4)">{{ moves[4] }}</td>
+        <td id="cell-6" @click="handleClick(5)">{{ moves[5] }}</td>
+      </tr>
+      <tr>
+        <td id="cell-7" @click="handleClick(6)">{{ moves[6] }}</td>
+        <td id="cell-8" @click="handleClick(7)">{{ moves[7] }}</td>
+        <td id="cell-9" @click="handleClick(8)">{{ moves[8] }}</td>
+      </tr>
+    </table>
+    <div>
+      <button @click="resetBoard">Reset</button>
+    </div>
+  </div>
+</template>
+
+<script>
+const EMPTY_BOARD = [null, null, null, null, null, null, null, null, null]
+const WINNING_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+export default {
+  data: () => ({
+    moves: [null, null, null, null, null, null, null, null, null],
+    nextPlayer: 'x',
+    hasWon: false,
+    invalidMove: false,
+  }),
+  methods: {
+    resetBoard () {
+      this.moves = JSON.parse(JSON.stringify(EMPTY_BOARD))
+      this.nextPlayer = 'x'
+      this.hasWon = false
+    },
+    handleClick (cellIndex) {
+      if (this.hasWon) {
+        return
+      }
+
+      if (this.moves[cellIndex] !== null) {
+        this.invalidMove = true
+        return
+      }
+
+      this.invalidMove = false
+      this.$set(this.moves, cellIndex, this.nextPlayer)
+
+      // Check for win
+      this.hasWon = this.checkForWinner(this.moves, this.nextPlayer)
+
+      this.nextPlayer = this.nextPlayer === 'x' ? 'o' : 'x'
+    },
+    checkForWinner (moves, currentPlayer) {
+      let hasWon = false
+
+      WINNING_COMBOS.forEach(function (combo) {
+        if (
+          moves[combo[0]] === currentPlayer &&
+          moves[combo[1]] === currentPlayer &&
+          moves[combo[2]] === currentPlayer
+        ) {
+          hasWon = true
+        }
+      })
+
+      return hasWon
+    },
+  },
+}
+</script>
+
+<style scoped>
+td {
+    border: #000000 1px solid;
+    width: 100px;
+    height: 100px;
+    text-align: center;
+    font-size: 50px;
+}
+
+#cell-1 {
+    border-top: none;
+    border-left: none;
+}
+
+#cell-2 {
+    border-top: none;
+}
+
+#cell-3 {
+    border-top: none;
+    border-right: none;
+}
+
+#cell-4 {
+    border-left: none;
+}
+
+#cell-6 {
+    border-right: none
+}
+
+#cell-7 {
+    border-bottom: none;
+    border-left: none;
+}
+
+#cell-8 {
+    border-bottom: none;
+}
+
+#cell-9 {
+    border-bottom: none;
+    border-right: none;
+}
+</style>
