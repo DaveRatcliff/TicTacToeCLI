ba8a5dc5806f50e5747f0622212522b8aeed5219
Wes Ratcliff <wes.ratcliff@gmail.com>
2018-04-04 10:22:50 -0700

    feat: Added SMS lambda function

---
+++ lambda-functions/send-game-invite.js
@@ -0,0 +1,61 @@
+const twilio = require('twilio')
+
+const accountSid = process.env.accountSid
+const authToken = process.env.authToken
+const from = process.env.from
+const client = new twilio(accountSid, authToken)
+
+exports.handler = async function ({ httpMethod, headers, body }, context, callback) {
+    
+    let { to, gameId } = body
+    let { 'x-api-key': apiKey } = headers
+
+    // TODO -- Check apiKey to make sure it's valid
+
+    if (!accountSid || !authToken || !from) {
+        return callback(null, {
+            statusCode: 400,
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ message: 'One of these environment variables are not set: accountSid, authToken, from' })
+        })
+    }
+
+    if (!gameId) {
+        return callback(null, {
+            statusCode: 400,
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ message: '"gameId" parameter not provided!' })
+        })
+    }
+    if (!to) {
+        return callback(null, {
+            statusCode: 400,
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ message: '"to" parameter not provided!' })
+        })
+    }
+
+    let message = `You're invited to play a game. Click here: http://site.com/${gameId}`
+
+    let messageId
+    try {
+        messageId = await sendSms(to, message, from)
+    } catch (e) {
+        return callback(e.toString())
+    }
+
+    return callback(null, {
+        statusCode: 200,
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ messageId }),
+    })
+}
+
+async function sendSms(to, body, from) {
+    const message = await client.messages.create({
+        body,
+        to,
+        from,
+    })
+    return message.sid
+}
